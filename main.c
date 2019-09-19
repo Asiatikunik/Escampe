cf9934b00bce1f7e7922f7ef1db704cbc8b04c39
BlackyStaar <duyunikdang@gmail.com>
2019-09-19 10:47:33 +0200

    second commit

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "graphics.h" 
+
+#define LARGEUR 700
+#define HAUTEUR 700
 
 int main(){
-
 	printf("Salut\n");
+	init_graphics(LARGEUR, HAUTEUR); /* (largeur, hauteur) */
+	POINT p;
+	p.x=250;
+	p.y=250;
+	draw_fill_circle(p, 100, aquamarine);
+	wait_escape();    
 	return 0;
 }
